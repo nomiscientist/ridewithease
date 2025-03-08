c66bb9c3588c0ec2728e35ae36526762dac5b51b
nomiscientist <scientistnomi@gmail.com>
2025-03-08 23:33:48 +0500

    cdn based assets added

---
+++ src/constants/assets.js
@@ -0,0 +1,6 @@
+// Asset URLs hosted on Cloudinary
+export const ASSETS = {
+  icon: "https://res.cloudinary.com/demo/image/upload/ridewithease/icon.png",
+  adaptiveIcon: "https://res.cloudinary.com/demo/image/upload/ridewithease/adaptive-icon.png",
+  splash: "https://res.cloudinary.com/demo/image/upload/ridewithease/splash.png"
+};
